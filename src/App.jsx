8044760892ddbc1ca01ade5df50d0346303658a0
Daniele <daniele.capa@gmail.com>
2024-05-16 10:55:43 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import NavBar from './components/NavBar'
 import Hero from "./components/Hero"
 import Featured from "./components/Featured"
 import DisplayWhole from './components/DisplayWhole'
+import { useState } from 'react'
 import { ContentfulClient, ContentfulProvider } from 'react-contentful'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  const client = new ContentfulClient({
+
+  const contentfulClient = new ContentfulClient({
     accessToken: import.meta.env.VITE_APP_CONTENTFUL_ACCESS_TOKEN,
     space: import.meta.env.VITE_APP_CONTENTFUL_SPACE_ID
-  })
+  });
+
+  const [contentful, setContentful] = useState([]);
 
   return (
     <div>
-      <NavBar />
-      <Hero />
-      <Featured />
-      <DisplayWhole />
+      <ContentfulProvider client={contentfulClient}>
+        <NavBar />
+        <Hero />
+        <Featured />
+        <DisplayWhole />
+      </ContentfulProvider>
     </div>
   )
 }
