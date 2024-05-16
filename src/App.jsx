dfd1615acd7859ea498557d665d7e12d3ab62a6a
Daniele <daniele.capa@gmail.com>
2024-05-16 09:51:44 +0200

    fetch in progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import NavBar from './components/NavBar'
 import Hero from "./components/Hero"
 import Featured from "./components/Featured"
 import DisplayWhole from './components/DisplayWhole'
+import { ContentfulClient, ContentfulProvider } from 'react-contentful'
 
 function App() {
-  
+  const client = new ContentfulClient({
+    accessToken: import.meta.env.VITE_APP_CONTENTFUL_ACCESS_TOKEN,
+    space: import.meta.env.VITE_APP_CONTENTFUL_SPACE_ID
+  })
 
   return (
     <div>
